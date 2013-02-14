70182e3acf6675a54573b88b19d0363b7b7aa692
Blake Mesdag <blakemesdag@gmail.com>
2013-02-14 10:58:37 -0500

    Add ssl support and remove dependency on mattr_accessor business

--- lib/gembeat.rb
+++ lib/gembeat.rb
@@ -1,12 +1,21 @@
-require "gembeat/version"
+##require "gembeat/version"
 require 'net/http'
 require 'uri'
 require 'json'
 require 'yaml'
 
 module Gembeat
-  mattr_accessor :token
-  mattr_accessor :pulse_url
+  [:token, :use_ssl, :pulse_url].each do |name|
+    define_method name do
+      class_eval "
+        @@#{name} = nil unless defined? @@#{name}
+
+        def self.#{name}
+          @@#{name}
+        end
+      "
+    end
+  end
 
   def self.specs
     @specs ||= Gem.loaded_specs
@@ -30,10 +39,25 @@ module Gembeat
     @uri ||= URI.parse(self.pulse_url)
   end
 
+  def self.ca_file
+    @cert ||= File.expand_path("../../data/ca_cert.pem", __FILE__)
+  end
+
+  def self.ca_file=(value)
+    @cert = value
+  end
+
   def self.send_pulse
     http = Net::HTTP.new(self.gembeat_uri.host, self.gembeat_uri.port)
     request = Net::HTTP::Post.new(self.gembeat_uri.request_uri, initheader = {'Content-Type' => "application/json"})
     request.body = self.application_hash.to_json
+
+    if self.use_ssl
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.ca_path = self.ca_file
+    end
+
     response = http.request(request)
   end
 end
